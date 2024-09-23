403f1784c99c4e060f8875dcd33c73de205385e3
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-09-23 21:19:56 +0530

    add bookcard

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,12 @@
 import Banner from "@/app/(home)/components/Banner";
 import Image from "next/image";
+import BookList from "./components/BookList";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <BookList />
     </>
   );
 }
